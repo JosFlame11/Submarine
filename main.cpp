155a389707d2e2cd820316665d0548ce490c23f7
Jose Lara <109773791+JosFlame11@users.noreply.github.com>
2022-12-12 00:35:38 -0600

    Movement

--- main.cpp
+++ main.cpp
@@ -1,116 +1,96 @@
-/*Se requiere agregar la libreria para la transmision de datos, se usara una distinta a RadioHead
-Agregar la lectura de la antena y con base a ello cambiar @/opc/
-*/
-
-
 #include <Arduino.h>
+#include <HardwareSerial.h>
 
-#define Ain1 2 
-#define Ain2 15 
-#define Bin1 16 
-#define Bin2 17
-//#define stby 4 conectar directamente a 3.3V
-#define PWMA 26
-#define PWMB 33
+#define in1 4
+#define in2 2
+#define PWM1 15
+#define in4 18
+#define in3 5
+#define PWM2 19
+#define in5 22
+#define in6 23
 
-#define in5 19
-#define in6 21
-
-#define LED 32
-#define Temp_pin 12
-#define PH_pin 14
-
-const int freq = 1000; //the frecuency of the motor, could be up to 1000hz
+const int vel = 175;
+const int freq = 2000; //the frecuency of the motor, could be up to 1000hz
 const int motorChannel_A = 0;
-const int motorChannel_B = 1;
+
 const int resolution = 8;
 
-const int velocity = 150; //from 0 to 255
-int opc = 0;
-
-void setup() {
-  pinMode(Ain1, OUTPUT);
-  pinMode(Ain2, OUTPUT);
-  pinMode(Bin1, OUTPUT);
-  pinMode(Bin2, OUTPUT);
-  ledcSetup(motorChannel_A, freq, resolution);
-  ledcSetup(motorChannel_B, freq, resolution);
-  ledcAttachPin(PWMA, motorChannel_A);
-  ledcAttachPin(PWMB, motorChannel_B);
-  //pinMode(PWMA, OUTPUT);
-  //pinMode(PWMB, OUTPUT);
-  pinMode(in5, OUTPUT);
-  pinMode(in6, OUTPUT);
-  pinMode(LED, OUTPUT);
-
-  pinMode(Temp_pin, INPUT);
-  pinMode(PH_pin, INPUT);
+int opc = 1;
+void foward(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
-
-void loop() {
-  //Si la antena recive 1
-  if (opc == 1){
-    foward();
-  }
-  //si recive 2
-  else if (opc == 2){
-    left();
-  }
-  //si recive 3
-  else if (opc == 3){
-    right();
-
-  }
-  //si recive 4
-
-  else if (opc == 4){
-    Down();
-  }
-  //Si recive 5
-  else if (opc == 5){
-    Up();
-  }
-  else{
-    opc = 0;
-  }
-  
+void left(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
-void foward(void){
-  ledcWrite(motorChannel_A, velocity);
-  ledcWrite(motorChannel_B, velocity);
-  digitalWrite(Ain1, HIGH);
-  digitalWrite(Ain2, LOW);
-  digitalWrite(Bin1, HIGH);
-  digitalWrite(Bin2, LOW);
+void right(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
-void left(void){
-  ledcWrite(motorChannel_A, velocity-50);
-  ledcWrite(motorChannel_B, velocity+50);
-  digitalWrite(Ain1, LOW);
-  digitalWrite(Ain2, HIGH);
-  digitalWrite(Bin1, HIGH);
-  digitalWrite(Bin2, LOW);
+void stop(){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, LOW);
 }
-void right(void){
-  ledcWrite(motorChannel_A, velocity+50);
-  ledcWrite(motorChannel_B, velocity-50);
-  digitalWrite(Ain1, HIGH);
-  digitalWrite(Ain2, LOW);
-  digitalWrite(Bin1, LOW);
-  digitalWrite(Bin2, HIGH);
-}
-void Down(){
+void up(){
   digitalWrite(in5, HIGH);
   digitalWrite(in6, LOW);
 }
-void Up(){
+
+void down(){
   digitalWrite(in5, LOW);
   digitalWrite(in6, HIGH);
 }
-//Lecturas
-void PH_Temp(){
-  int t = analogRead(Temp_pin);
-  int ph = digitalRead(PH_pin);
-  int voltage = ph*(3.3 / 1023.0);
-  float ph_value = -5.70 *(voltage + 21.34);
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(in5, OUTPUT);
+  pinMode(in6, OUTPUT);
+  
+  ledcSetup(motorChannel_A, freq, resolution);
+  ledcAttachPin(PWM1, motorChannel_A);
+  ledcAttachPin(PWM2, motorChannel_A);
+
+}
+
+void loop() {
+  ledcWrite(motorChannel_A, vel);
+  if (opc == 1)
+  {
+    foward();
+  }
+  else if (opc == 2)
+  {
+    left();
+  }
+  else if (opc == 3)
+  {
+    right();
+  } 
+  else if (opc == 4)
+  {
+    down(); 
+  }
+  else if (opc == 5)
+  {
+    up();
+  }
+  
+  else {
+    stop();
+  }
+
 }
